81fa10b5be2b99fdd0bb6f9d4202d0a9a844c5f5
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-14 19:20:09 -0500

    Added vertical search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (matrix) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
 
-module.exports = wordSearch
+  const transposedMatrix = [];
+  for (let j = 0; j < numCols; j++) {
+    const newRow = [];
+    for (let i = 0; i < numRows; i++) {
+      newRow.push(matrix[i][j]);
+    }
+    transposedMatrix.push(newRow);
+  }
+
+  return transposedMatrix;
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  if (word.length === 0) {
+    return false;
+  } else {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+
+    // Vertical Search
+    let transLetters = transpose(letters);
+    const verticalJoin = transLetters.map((ls) => ls.join(""));
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
